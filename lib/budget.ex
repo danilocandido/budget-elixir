9d2f744978cd2ee9533346a6cb985f36d21f8d26
Danilo Cândido <danilod07@gmail.com>
2018-03-19 18:20:14 -0300

    adding methods

--- lib/budget.ex
+++ lib/budget.ex
@@ -1,5 +1,36 @@
 defmodule Budget do
-  def hello do
-    :world
+  alias NimbleCSV.RFC4180, as: CSV
+
+  def start(_type, _args) do
+    list_transactions()
+  end
+
+  def list_transactions do
+    File.read!("lib/transactions-jan.csv")
+    |> parse
+    |> filter
+    |> normalize
+  end
+
+  defp parse(string) do
+    string
+    |> String.replace("\r", "")
+    |> CSV.parse_string
+  end
+
+  defp filter(rows) do
+    Enum.map(rows, &Enum.drop(&1, 1))
+  end
+
+  defp normalize(rows) do
+    Enum.map(rows, &parse_amount(&1))
+  end
+
+  defp parse_amount([date, description, amount]) do
+    [date, description, parse_to_float(amount)]
+  end
+
+  defp parse_to_float(string) do
+    String.to_float(string)
   end
 end
